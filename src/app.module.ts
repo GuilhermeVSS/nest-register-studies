ec4110972c9ca6743ef3edb7ea83cf0c6e4c78f5
Guilherme Ventura [gvss] <38739201+GuilhermeVSS@users.noreply.github.com>
2025-07-26 18:14:11 -0300

    feat(app.module/main.ts): update `app.module` and `main.ts` to use `ProducerModule` as a import and to use openApi

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaModule } from 'prisma/prisma.module';
+import { PrismaModule } from '../prisma/prisma.module';
 import { ProducerModule } from './producer/producer.module';
 import { ConfigModule } from '@nestjs/config';
 
